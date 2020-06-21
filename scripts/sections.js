905101b58dfef8743e996fe02adf56c14a701689
zitj <1eyedea2eyedeas@gmail.com>
2020-06-21 17:01:16 +0200

    swiper.js added

--- scripts/sections.js
+++ scripts/sections.js
@@ -1,11 +1,15 @@
 const nav = document.querySelector('nav');
 const contact = document.querySelector('.contact');
+const graphicDesign = document.querySelector('.swiper-container');
 const sectionButtons = nav.querySelectorAll('a');
 
 const buttonClassRemoval = () =>{
     for(let i = 0; i < sectionButtons.length; i++){
         sectionButtons[i].classList.remove('active');
     }
+}
+const classRemoval = () =>{
+    graphicDesign.classList.add('none');
 }
 
 for(let i = 0; i < sectionButtons.length; i++){
@@ -14,12 +18,16 @@ for(let i = 0; i < sectionButtons.length; i++){
         sectionButtons[i].classList.add('active');
         if(sectionButtons[0].classList.contains('active')){
             contact.classList.add('none');
+            graphicDesign.classList.add('none');
         }
         if(sectionButtons[1].classList.contains('active')){
             contact.classList.add('none');
+            graphicDesign.classList.remove('none');
         }
         if(sectionButtons[2].classList.contains('active')){
+            graphicDesign.classList.add('none');
             contact.classList.remove('none');
         }
     });
 }
+setTimeout(classRemoval, 0);
