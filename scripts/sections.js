866f496950affc941e2116ed04d219a4315dba80
Aleksandar Veljkovic <1eyedea2eyedeas@gmail.com>
2023-05-14 18:35:58 +0200

    Reordered, renamed files and commented out line for swipper scrollbar

--- scripts/sections.js
+++ scripts/sections.js
@@ -13,54 +13,55 @@ const graphicDesignSection = swiperContainer[0];
 const webDesignSection = swiperContainer[1];
 
 const buttonClassRemoval = () => {
-    for (let i = 0; i < sectionButtons.length; i++) {
-        sectionButtons[i].classList.remove('active');
-    }
+	for (let i = 0; i < sectionButtons.length; i++) {
+		sectionButtons[i].classList.remove('active');
+	}
 };
 const swipeIlustrationAnimation = () => {
-    for (let i = 0; i < swipeIlustration.length; i++) {
-        swipeIlustration[i].classList.remove('none');
-        swipeIlustration[i].addEventListener('animationend', () => {
-            swipeIlustration[i].classList.add('none');
-        });
-    }
+	for (let i = 0; i < swipeIlustration.length; i++) {
+		swipeIlustration[i].classList.remove('none');
+		swipeIlustration[i].addEventListener('animationend', () => {
+			swipeIlustration[i].classList.add('none');
+		});
+	}
 };
 const classRemoval = () => {
-    graphicDesignSection.classList.add('none');
-    webDesignSection.classList.add('none');
+	graphicDesignSection.classList.add('none');
+	webDesignSection.classList.add('none');
 };
 const contactIntroAnimationTiming = () => {
-    contactSection.classList.remove('none');
+	contactSection.classList.remove('none');
 };
 const introAnimationEvent = () => {
-    intro.classList.remove('none');
-    intro.classList.add('active');
+	intro.classList.remove('none');
+	intro.classList.add('active');
 };
+
 for (let i = 0; i < sectionButtons.length; i++) {
-    sectionButtons[i].addEventListener('click', () => {
-        buttonClassRemoval();
-        sectionButtons[i].classList.add('active');
-        if (sectionButtons[0].classList.contains('active')) {
-            contactSection.classList.add('none');
-            graphicDesignSection.classList.add('none');
-            webDesignSection.classList.remove('none');
-            swipeIlustrationAnimation();
-        }
-        if (sectionButtons[1].classList.contains('active')) {
-            contactSection.classList.add('none');
-            graphicDesignSection.classList.remove('none');
-            webDesignSection.classList.add('none');
-            swipeIlustrationAnimation();
-        }
-        if (sectionButtons[2].classList.contains('active')) {
-            classRemoval();
-            contactSection.classList.remove('none');
-        }
-    });
+	sectionButtons[i].addEventListener('click', () => {
+		buttonClassRemoval();
+		sectionButtons[i].classList.add('active');
+		if (sectionButtons[0].classList.contains('active')) {
+			contactSection.classList.add('none');
+			graphicDesignSection.classList.add('none');
+			webDesignSection.classList.remove('none');
+			swipeIlustrationAnimation();
+		}
+		if (sectionButtons[1].classList.contains('active')) {
+			contactSection.classList.add('none');
+			graphicDesignSection.classList.remove('none');
+			webDesignSection.classList.add('none');
+			swipeIlustrationAnimation();
+		}
+		if (sectionButtons[2].classList.contains('active')) {
+			classRemoval();
+			contactSection.classList.remove('none');
+		}
+	});
 }
 introTop.addEventListener('animationend', () => {
-    intro.classList.remove('active');
-    intro.classList.add('none');
+	intro.classList.remove('active');
+	intro.classList.add('none');
 });
 
 setTimeout(contactIntroAnimationTiming, 10);
--- scripts/swipe.js
+++ scripts/swipe.js
@@ -1,32 +1,32 @@
-let mySwiper = new Swiper ('.swiper-container', {
-    // Optional parameters
-    // direction: 'vertical',
-    // loop: true,
-    effect: 'coverflow',
-      grabCursor: true,
-      centeredSlides: true,
-      slidesPerView: 'auto',
-      coverflowEffect: {
-        rotate: 50,
-        stretch: 0,
-        depth: 100,
-        modifier: 1,
-        slideShadows: true,
-      
-    // If we need pagination
-    // pagination: {
-    //   el: '.swiper-pagination',
-    // },
+// let mySwiper = new Swiper ('.swiper-container', {
+//     // Optional parameters
+//     // direction: 'vertical',
+//     // loop: true,
+//     effect: 'coverflow',
+//       grabCursor: true,
+//       centeredSlides: true,
+//       slidesPerView: 'auto',
+//       coverflowEffect: {
+//         rotate: 50,
+//         stretch: 0,
+//         depth: 100,
+//         modifier: 1,
+//         slideShadows: true,
 
-    // // Navigation arrows
-    navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev',
-    }
+//     // If we need pagination
+//     // pagination: {
+//     //   el: '.swiper-pagination',
+//     // },
 
-    // // And if we need scrollbar
-    // scrollbar: {
-    //   el: '.swiper-scrollbar',
-    // },
-    }
-  })
+//     // // Navigation arrows
+//     navigation: {
+//       nextEl: '.swiper-button-next',
+//       prevEl: '.swiper-button-prev',
+//     }
+
+//     // // And if we need scrollbar
+//     // scrollbar: {
+//     //   el: '.swiper-scrollbar',
+//     // },
+//     }
+//   })
